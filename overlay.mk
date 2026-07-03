873303477874cf09dd89bfc19ba15de4c858f207
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-03 23:46:14 +0900

    remove useless things

--- overlay.mk
+++ overlay.mk
@@ -285,8 +285,6 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-j6 \
 	treble-overlay-samsung-m23xq \
 	treble-overlay-samsung-m23xq-systemui \
-	treble-overlay-samsung-m23xq-wifi \
-	treble-overlay-samsung-m23xq-settingsappv2 \
 	treble-overlay-samsung-m31 \
 	treble-overlay-samsung-m31-systemui \
 	treble-overlay-samsung-m31s \
